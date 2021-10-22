aeadbe340ecf200da22f8130312aaf02456d1407
CRBroughton <crbroughton@posteo.uk>
2021-10-22 15:32:15 +0100

    added clearRect function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ let canvas = <HTMLCanvasElement> document.getElementById("gameScreen");
 
 let ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
 
+ctx.clearRect(0, 0, 800, 600)
 
 ctx.fillStyle = <string> '#f00';
 ctx.fillRect(20, 20, 100, 100);
