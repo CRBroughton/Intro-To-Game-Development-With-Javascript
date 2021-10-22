e4cf04c01d183d7fa6596a56d5311b181c37ceda
CRBroughton <crbroughton@posteo.uk>
2021-10-22 15:31:24 +0100

    drew second square

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ let canvas = <HTMLCanvasElement> document.getElementById("gameScreen");
 let ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
 
 
-ctx.fillStyle = '#f00';
+ctx.fillStyle = <string> '#f00';
 ctx.fillRect(20, 20, 100, 100);
+
+ctx.fillStyle = <string> '#00f';
+ctx.fillRect(200, 200, 50, 50);
