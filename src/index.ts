6fdcea6b6d6e3476976d7952d65df3a2a35e85f8
CRBroughton <crbroughton@posteo.uk>
2021-10-22 15:29:45 +0100

    drew square on screen

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let canvas = <HTMLCanvasElement> document.getElementById("gameScreen");
+
+let ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
+
+
+ctx.fillStyle = '#f00';
+ctx.fillRect(20, 20, 100, 100);
