89cd996ccadb7a9f17320f986ec95cde4733bcd9
CRBroughton <crbroughton@posteo.uk>
2021-10-31 15:20:52 +0000

    game now has lives

--- src/game.ts
+++ src/game.ts
@@ -19,15 +19,16 @@ export default class Game {
     gameHeight: number;
     paddle!: Paddle;
     ball!: Ball;
+    lives: number;
     gameObjects: (Paddle | Ball | Brick | markedForDeletion) [] = [];
     constructor(gameWidth: number, gameHeight: number) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.gamestate = GAMESTATE.RUNNING;
         this.gamestate = GAMESTATE.MENU;
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
         this.gameObjects = [];
+        this.lives = 3;
 
         new InputHandler(this.paddle, this)
 
@@ -48,8 +49,12 @@ export default class Game {
 
     update(deltaTime: number) {
 
+        if(this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER
+
         if(this.gamestate === GAMESTATE.PAUSED ||
-           this.gamestate === GAMESTATE.MENU) return;
+           this.gamestate === GAMESTATE.MENU ||
+           this.gamestate === GAMESTATE.GAMEOVER)
+            return;
 
         this.gameObjects.forEach(object => object.update(deltaTime));
 
@@ -79,6 +84,17 @@ export default class Game {
             ctx.fillStyle = "white"
             ctx.textAlign = "center"
             ctx.fillText("Press SPACEBAR To Start", this.gameWidth / 2, this.gameHeight / 2)
+        }
+
+        if(this.gamestate === GAMESTATE.GAMEOVER) {
+            ctx.rect(0, 0, this.gameWidth, this.gameHeight)
+            ctx.fillStyle = "rgba(0,0,0,1)"
+            ctx.fill()
+
+            ctx.font = "30px Arial"
+            ctx.fillStyle = "white"
+            ctx.textAlign = "center"
+            ctx.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2)
         }
     }
 
