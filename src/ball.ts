8a17122e3fe3d01acae2a352b96ca2e86874641e
CRBroughton <crbroughton@posteo.uk>
2021-10-31 13:06:19 +0000

    defined ball position and speed

--- src/ball.ts
+++ src/ball.ts
@@ -2,13 +2,25 @@ import Position from './interfaces/Position.js'
 
 export default class Ball {
     image: HTMLImageElement
+    speed: object
+    position: Position;
     constructor() {
         this.image = <HTMLImageElement>document.getElementById('img_ball')!
+        this.position = {
+            x: 10,
+            y: 10,
+        }
+        this.speed = {
+            x: 2,
+            y: 2,
+        }
     }
 
     draw(ctx: CanvasRenderingContext2D) {
         ctx.drawImage(this.image, 10, 10, 16, 16)
     }
 
-    update() {}
+    update(deltaTime) {
+
+    }
 }
