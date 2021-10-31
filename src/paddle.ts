bca688d98f774f20126513adb2e28e46acadf9d9
CRBroughton <crbroughton@posteo.uk>
2021-10-31 10:57:56 +0000

    finished paddle.ts

--- src/paddle.ts
+++ src/paddle.ts
@@ -1,9 +1,22 @@
-class Paddle {
-    constructor() {
+import { Position } from './interfaces/Position.js'
 
+export default class Paddle {
+
+    width: number;
+    height: number;
+    position: Position;
+    
+    constructor(gameWidth: number, gameHeight: number) {
+        this.width = 150;
+        this.height = 30;
+
+        this.position = {
+            x: gameWidth / 2 - this.width / 2,
+            y: gameHeight - this.height - 10
+        }
     }
 
-    draw(ctx) {
-        
+    draw(ctx: { fillRect: (arg0: number, arg1: number, arg2: number, arg3: number) => void; }) {
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
