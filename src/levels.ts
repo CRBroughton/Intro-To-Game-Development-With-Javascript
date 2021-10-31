a3f9dba195c4e367da807d0026e8ea71202bb1d1
CRBroughton <crbroughton@posteo.uk>
2021-10-31 14:33:47 +0000

    created buildLevel function

--- src/levels.ts
+++ src/levels.ts
@@ -1,3 +1,23 @@
+import Brick from './brick.js'
+
+export function buildLevel(game: any, level: any[]) {
+    let bricks: Brick[] = []
+
+    level.forEach((row, rowIndex) => {
+        row.forEach((brick: number, brickIndex: number) => {
+            if(brick === 1) {
+                let position = {
+                    x: 80 * brickIndex,
+                    y: 20 + 24 * rowIndex,
+                }
+                bricks.push(new Brick(game, position))
+            }
+        })
+    })
+
+    return bricks;
+}
+
 export const level1 = [
     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
