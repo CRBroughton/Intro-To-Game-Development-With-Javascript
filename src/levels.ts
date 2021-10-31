3e3fa1942d6c9e9a878bc12760bf134ac763ee35
CRBroughton <crbroughton@posteo.uk>
2021-10-31 14:36:19 +0000

    adjusted top of brick space

--- src/levels.ts
+++ src/levels.ts
@@ -8,7 +8,7 @@ export function buildLevel(game: any, level: number[][]) {
             if(brick === 1) {
                 let position = {
                     x: 80 * brickIndex,
-                    y: 20 + 24 * rowIndex,
+                    y: 75 + 24 * rowIndex,
                 }
                 bricks.push(new Brick(game, position))
             }
