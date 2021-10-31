559b0178b2ff915264aa2843fe431cf6be41747a
CRBroughton <crbroughton@posteo.uk>
2021-10-31 13:02:19 +0000

    moved ball to class

---
+++ src/ball.ts
@@ -0,0 +1,12 @@
+export default class Ball {
+    image: HTMLImageElement
+    constructor() {
+        this.image = <HTMLImageElement>document.getElementById('img_ball')!
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.drawImage(this.image, 10, 10, 16, 16)
+    }
+
+    update() {}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import Paddle from './paddle.js'
 import InputHandler from './input.js'
+import Ball from './ball.js'
 
 let canvas = <HTMLCanvasElement> document.getElementById("gameScreen");
 
 let ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
 
 const GAME_WIDTH = 800;
-const GAME_HEIGHT= 600;
+const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball()
 
 new InputHandler(paddle)
 
 let lastTime: number = 0;
-
-// images
-
-let imgBall = <HTMLImageElement>document.getElementById('img_ball')!
 
 function gameLoop(timeStamp: number) {
 
@@ -24,7 +22,7 @@ function gameLoop(timeStamp: number) {
     lastTime = timeStamp;
 
     // Clears the canvas after every frame
-    ctx.clearRect(0, 0, 800, 600)
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime);
     paddle.draw(ctx);
 
